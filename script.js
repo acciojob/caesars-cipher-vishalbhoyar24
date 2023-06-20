87f9ba815233a4e523905db69753b5ab59e5f547
vishalbhoyar24 <124866977+vishalbhoyar24@users.noreply.github.com>
2023-06-20 17:09:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,24 @@
 // Your Script here.
+// Your Script here.
+function rot13(str){
+	let newStr = "";
+	for(let i = 0; i < str.length; i++){
+		if(str.charCodeAt(i)>= 65 && str.charCodeAT(i) <= 90){
+			for(let i = 1; i <= 13; i++){
+				if(str.charCodeAt(i) < 90){
+					str.charCodeAt(i) += 1;
+				}else{
+					str.charCodeAt(i) = 65;
+				}
+			}
+			let newValue = str.charCodeAt(i);
+			newStr += String.fromCharCode(newValue);
+		}else{
+			newStr += str.charAt(i);
+		}
+	}
+	return newStr;
+}
 
 const lookup = {
   A: "N",
