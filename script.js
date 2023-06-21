901bd12d67a447bd90da8b7401e55c6bc3f745fd
Umakant Dwivedi <78226651+ukdgit@users.noreply.github.com>
2023-06-21 15:07:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -51,12 +51,12 @@ const lookup = {
   ",": ",",
 };
 
-function rot13(encodedStr) {
-  let decodedArr = []; // Your Result goes here
-  // Only change code below this line
+// function rot13(encodedStr) {
+//   let decodedArr = []; // Your Result goes here
+//   // Only change code below this line
 
-  return; //return decodedArr
-}
+//   return; //return decodedArr
+// }
 
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
